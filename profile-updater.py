293a33d67535fcf9b124801db274c03bb8f2de1b
Devin <devin.smith.work@gmail.com>
2023-08-03 13:31:44 -0700

    Added warning when user record does not exist

--- profile-updater.py
+++ profile-updater.py
@@ -1,3 +1,6 @@
+# NOTE! This program is designed for Elements API v 5.5
+# Modifications may need to be made if we've updated to a newer version.
+
 import creds
 
 from csv import DictReader
@@ -57,12 +60,26 @@ def retrieve_user_record_ids(ud):
                                "User ID": item["User ID"]
                                } for item in rows}
 
-    for ud_row in ud:
-        ud_row["user_record_id"] = prop_id_keys_dicts[ud_row["user_proprietary_id"]]["User Record ID"]
-        ud_row["user_id"] = prop_id_keys_dicts[ud_row["user_proprietary_id"]]["User ID"]
+    # Loop through the user dicts.
+    # If there's no matching "Proprietary ID" in the SQL results, set to None (filters during the return)
+    # Otherwise, add the SQL results to the dict.
+    for index, user_dict in enumerate(ud, 0):
+        if user_dict["user_proprietary_id"] not in prop_id_keys_dicts.keys():
+            print("\nWARNING: A user in the CSV does not have a manual 'User Record' in the reporting DB. This likely "
+                  "indicates they are a new user. They will be skipped for updating until they have a manual user "
+                  "record -- which we believe occurs when they are created in Elements, either manually or via an HR "
+                  "feed update.")
+            print("user_proprietary_id:", user_dict['user_proprietary_id'])
+            ud[index] = None
+
+        else:
+            user_dict["user_record_id"] = prop_id_keys_dicts[user_dict["user_proprietary_id"]]["User Record ID"]
+            user_dict["user_id"] = prop_id_keys_dicts[user_dict["user_proprietary_id"]]["User ID"]
 
     cursor.close()
     conn.close()
+
+    return [user_dict for user_dict in ud if user_dict is not None]
 
 
 # -----------------------------
@@ -83,12 +100,8 @@ def create_xml_bodies(ud):
     # </update-record>
 
     # Main XML function
-    for row in ud:
-
-        # We need the record ID to update the record.
-        # TK TK -- new hires may not have records? And may need PUT rather than PATCH? Discuss!
-        if "user_record_id" not in row.keys():
-            continue
+    for user_dict in ud:
+        # if "user_record_id" not in user_dict.keys(): continue
 
         # XML root <update-record> and child node <fields>
         root = ET.Element('update-record', xmlns="http://www.symplectic.co.uk/publications/api")
@@ -103,12 +116,12 @@ def create_xml_bodies(ud):
 
         # Create an XML node for each of the user's non-empty fields
         for field_name in update_fields:
-            if row[field_name] != "":
+            if user_dict[field_name] != "":
                 field = ET.SubElement(fields, "field", name=field_name, operation="set")
-                ET.SubElement(field, "text").text = row[field_name]
+                ET.SubElement(field, "text").text = user_dict[field_name]
 
         # Convert XML object to string.
-        row["xml"] = ET.tostring(root)
+        user_dict["xml"] = ET.tostring(root)
 
 
 # -----------------------------
@@ -154,7 +167,8 @@ def update_records_via_api(ud):
 qa_mode = True
 ssh_tunnel_needed = True
 
-# Loads appropriate creds based on the above flags
+# Loads creds based on the above flags
+# --------- QA
 if qa_mode:
     ssh_creds = creds.ssh_creds_qa
     api_creds = creds.api_creds_qa
@@ -164,6 +178,8 @@ if qa_mode:
     else:
         sql_creds = creds.sql_creds_server_qa
         sql_driver = '{ODBC Driver 17 for SQL Server}'
+
+# --------- PROD
 else:
     ssh_creds = creds.ssh_creds
     api_creds = creds.api_creds
@@ -191,8 +207,9 @@ if ssh_tunnel_needed:
 # Convert the CSV to dict.
 updates_dict = convert_update_csv("Bulk_Profile_Test_2.csv")
 
-# Reporting DB: Add the user record IDs to the dict
-retrieve_user_record_ids(updates_dict)
+# Reporting DB: Add the user record IDs to the dict,
+# Filters for any users not yet in the system
+updates_dict = retrieve_user_record_ids(updates_dict)
 
 # Adds the xml bodies for the update procedure
 create_xml_bodies(updates_dict)
